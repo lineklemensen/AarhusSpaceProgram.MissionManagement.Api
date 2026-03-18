450d0c5c32f5b22c3889b8f2c5a80b3121b53d07
lineklemensen <202407497@post.au.dk>
2026-03-18 19:49:35 +0100

    Added DbCOntext file with boilerplate code o begin the data model

---
+++ AarhusSpaceProgram.MissionManagement.Api/Models/MissionManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AarhusSpaceProgram.MissionManagement.Api.Models
+{
+    public class MissionManagementDbContext : DbContext
+    {
+        public MissionManagementDbContext(
+            DbContextOptions<MissionManagementDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
