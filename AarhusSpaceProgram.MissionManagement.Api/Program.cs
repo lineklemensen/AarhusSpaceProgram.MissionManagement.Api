f8eaa76ad7eb489d796d86cb064d5dd9bda59dc0
lineklemensen <202407497@post.au.dk>
2026-03-19 16:22:27 +0100

    Add EF Core and SQL Server support to project
    Added Entity Framework Core base and SQL Server provider packages. Registered MissionManagementDbContext with DI in Program.cs, configured to use SQL Server via "DefaultConnection". Prepares project for SQL Server database integration.

--- AarhusSpaceProgram.MissionManagement.Api/Program.cs
+++ AarhusSpaceProgram.MissionManagement.Api/Program.cs
@@ -1,4 +1,6 @@
 using Scalar.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using AarhusSpaceProgram.MissionManagement.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<MissionManagementDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
 
 var app = builder.Build();
 
