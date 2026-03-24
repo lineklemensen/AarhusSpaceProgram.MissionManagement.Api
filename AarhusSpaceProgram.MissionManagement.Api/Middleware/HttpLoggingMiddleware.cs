73673f7ca20a03e7c3355c0a221e925af4e790d1
lineklemensen <202407497@post.au.dk>
2026-03-24 13:00:58 +0100

    Refactor Serilog config, add HttpLoggingMiddleware stub
    - Updated Serilog config to use expression filters and log only 'ASPMiddleware.HttpLogs' entries to MongoDB; raised minimum log level to Warning.
    - Upgraded Scalar.AspNetCore and added Serilog.Expressions dependency.
    - Introduced initial HttpLoggingMiddleware class with logger setup.

---
+++ AarhusSpaceProgram.MissionManagement.Api/Middleware/HttpLoggingMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace AarhusSpaceProgram.MissionManagement.Api.Middleware
+{
+    public sealed class HttpLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _asplogger;
+
+        public HttpLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _asplogger = loggerFactory.CreateLogger("ASPMiddleware.HttpLogs");
+        }
+
+    }
+}
