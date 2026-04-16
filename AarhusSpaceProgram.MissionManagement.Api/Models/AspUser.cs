91cea5405f7113a3eec5d81db999cda8baff59c4
lineklemensen <202407497@post.au.dk>
2026-04-16 12:46:02 +0200

    Commit before handling package problems. Implemented basic Identityuser

---
+++ AarhusSpaceProgram.MissionManagement.Api/Models/AspUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AarhusSpaceProgram.MissionManagement.Api.Models
+{
+    public class AspUser : IdentityUser
+    {
+    }
+}
--- AarhusSpaceProgram.MissionManagement.Api/Models/MissionManagementDbContext.cs
+++ AarhusSpaceProgram.MissionManagement.Api/Models/MissionManagementDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using AarhusSpaceProgram.MissionManagement.Api.Models.Enums;
 
 namespace AarhusSpaceProgram.MissionManagement.Api.Models
 {
-    public class MissionManagementDbContext : DbContext
+    public class MissionManagementDbContext : IdentityDbContext<AspUser>
     {
         public MissionManagementDbContext(
             DbContextOptions<MissionManagementDbContext> options)
