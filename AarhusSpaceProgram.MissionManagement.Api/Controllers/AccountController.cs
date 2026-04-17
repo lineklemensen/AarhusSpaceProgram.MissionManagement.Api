684b42d0b3736625b1a454cfcd24b61f02318ba7
lineklemensen <202407497@post.au.dk>
2026-04-17 17:41:56 +0200

    Created AccountController class with dependency injection

---
+++ AarhusSpaceProgram.MissionManagement.Api/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AarhusSpaceProgram.MissionManagement.Api.DTO;
+using AarhusSpaceProgram.MissionManagement.Api.Models;
+using System.Linq.Expressions;
+using System.Linq.Dynamic.Core;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AarhusSpaceProgram.MissionManagement.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly MissionManagementDbContext _context;
+
+        private readonly ILogger<AccountController> _logger;
+
+        private readonly IConfiguration _configuration;
+
+        private readonly UserManager<AspUser> _userManager;
+
+        private readonly SignInManager<AspUser> _signInManager;
+
+        public AccountController(
+            MissionManagementDbContext context,
+            ILogger<AccountController> logger,
+            IConfiguration configuration,
+            UserManager<AspUser> userManager,
+            SignInManager<AspUser> signInManager)
+        {
+            _context = context;
+            _logger = logger;
+            _configuration = configuration;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
