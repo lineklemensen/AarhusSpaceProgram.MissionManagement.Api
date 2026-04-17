30b0a01543d40f7422efca6ee4dc76b101fdd498
lineklemensen <202407497@post.au.dk>
2026-04-17 18:33:52 +0200

    Register method created and tested with a test user.

--- AarhusSpaceProgram.MissionManagement.Api/Controllers/AccountController.cs
+++ AarhusSpaceProgram.MissionManagement.Api/Controllers/AccountController.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using AarhusSpaceProgram.MissionManagement.Api.DTO;
 using AarhusSpaceProgram.MissionManagement.Api.Models;
 using System.Linq.Expressions;
-using System.Linq.Dynamic.Core;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
@@ -39,6 +38,62 @@ namespace AarhusSpaceProgram.MissionManagement.Api.Controllers
             _configuration = configuration;
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        // [ResponseCache(CacheProfileName = "NoCache")]
+        public async Task<ActionResult> Register(RegisterDTO input)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var newUser = new AspUser();
+
+                    newUser.UserName = input.UserName;
+                    newUser.Email = input.Email;
+
+                    var result = await _userManager.CreateAsync(
+                        newUser, input.Password);
+
+                    if (result.Succeeded)
+                    {
+                        return StatusCode(201,
+                            $"User '{newUser.UserName}' has been created");
+                    }
+                    else
+                        throw new Exception(
+                            string.Format("Error: {0}", string.Join(" ",
+                                result.Errors.Select(e => e.Description))));
+                }
+                else
+                {
+                    var details = new ValidationProblemDetails(ModelState);
+
+                    details.Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+                    details.Status = StatusCodes.Status400BadRequest;
+                    
+                    return new BadRequestObjectResult(details);
+                }
+            }
+            catch (Exception e)
+            {
+                var exceptionDetails = new ProblemDetails();
+
+                exceptionDetails.Detail = e.Message;
+                exceptionDetails.Status = StatusCodes.Status500InternalServerError;
+                exceptionDetails.Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError, exceptionDetails);
+            }
+        }
+
+        [HttpPost]
+        // [ResponseCache(CacheProfileName = "NoCache")]
+        public async Task<ActionResult> Login()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- AarhusSpaceProgram.MissionManagement.Api/Program.cs
+++ AarhusSpaceProgram.MissionManagement.Api/Program.cs
@@ -51,6 +51,7 @@ builder.Services.AddIdentity<AspUser, IdentityRole>(options =>
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
     options.Password.RequiredLength = 8;
 })
     .AddEntityFrameworkStores<MissionManagementDbContext>();
